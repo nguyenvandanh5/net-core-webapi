c4728af1d50a767efdb3d973a49d3378ba761450
Nguyen Van Danh <vandanh1308@gmail.com>
2023-02-27 21:47:13 +0700

    Getting Started NET core Api

---
+++ MyWebApiApp/MyWebApiApp/Controllers/ProductsController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Models;
+using MyWebApiApp.Repositories;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepo;
+
+        public ProductsController(IBookRepository repo)
+        {
+            _bookRepo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            try
+            {
+                return Ok(await _bookRepo.GetAllBooksAsync());
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBookById(int id)
+        {
+            try
+            {
+                var book = await _bookRepo.GetBookAsync(id); 
+                return book == null ? NotFound() : Ok(book);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewBook(BookModel model)
+        {
+            try
+            {
+                var newBookId = await _bookRepo.AddBooksAsync(model);
+                var book = await _bookRepo.GetBookAsync(newBookId);
+                return book == null ? NotFound() : Ok(book);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, BookModel model)
+        {
+            try
+            {
+                if (id != model.Id)
+                {
+                    return NotFound();
+                }
+                await _bookRepo.UpdateBookAsync(id, model);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            try
+            {
+                await _bookRepo.DeleteBookAsync(id);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Data/BookStroreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApiApp.Data
+{
+    public class BookStroreContext : DbContext
+    {
+        public BookStroreContext(DbContextOptions<BookStroreContext> opt): base(opt) 
+        { 
+
+        }
+
+        #region Dbset
+        public DbSet<Book>? Books { get; set; }
+        #endregion
+    }
+}
